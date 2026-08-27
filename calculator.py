8a1221432e516325fa137f148c6d3b6c379179f1
Kunal483-dev <rajpootkunal969@gmail.com>
2026-08-27 12:32:53 +0530

    first upload

---
+++ calculator.py
@@ -0,0 +1,19 @@
+# User se input lena
+num1 = float(input("Pehla number dalein: "))
+operator = input("Operator dalein (+, -, *, /): ")
+num2 = float(input("Doosra number dalein: "))
+
+# Calculation aur result
+if operator == '+':
+    print("Result:", num1 + num2)
+elif operator == '-':
+    print("Result:", num1 - num2)
+elif operator == '*':
+    print("Result:", num1 * num2)
+elif operator == '/':
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Error: Zero se divide nahi kar sakte!")
+else:
+    print("Galat operator!")
